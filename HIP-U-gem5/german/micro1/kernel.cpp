ad0626ac39ccca8b9477c91f0b6713c2df108934
german-v-t-um-es <german.v.t@um.es>
2024-11-12 13:24:10 +0100

    Micro1 v4

--- HIP-U-gem5/german/micro1/kernel.cpp
+++ HIP-U-gem5/german/micro1/kernel.cpp
@@ -7,10 +7,10 @@
 // CPU threads-----------------------------------------------------------------
 void run_cpu_threads(int size, int elements_gpu, int n_threads, float* h_in, float* h_out) {
 
-    int elements_thread = size/n_threads;
     std::vector<std::thread> cpu_threads;
     for(int k = 0; k < n_threads; k++) {
         cpu_threads.push_back(std::thread([=]() {
+            int elements_thread = size/n_threads;
             if(k+1==n_threads)
                 elements_thread += size%n_threads;
             for(int i=elements_thread; i<elements_thread; i++){
