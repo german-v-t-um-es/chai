0efb1b6767a68221e96d47da4dcec0805fa08f4e
german-v-t-um-es <german.v.t@um.es>
2024-11-13 13:14:28 +0100

    Micro1 v15

--- HIP-U-gem5/german/micro1/main.cpp
+++ HIP-U-gem5/german/micro1/main.cpp
@@ -111,8 +111,8 @@ int main(int argc, char **argv) {
     float*  h_in  = (float*) malloc(array_size);
     float*  h_out = (float*) malloc(array_size);
     // Division of the elements for each device (CPU, GPU)
-    int n_elements_gpu = p.size * p.alpha;
-    int n_elements_cpu = p.size - n_elements_gpu;
+    int n_elements_cpu = p.size * p.alpha;
+    int n_elements_gpu = p.size - n_elements_gpu;
     fprintf(stderr, "GPU elements to process: %d\n", n_elements_gpu);
     fprintf(stderr, "CPU elements to process: %d\n", n_elements_cpu);
     
